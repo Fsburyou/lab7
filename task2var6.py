d65039673fd68c1c9135e5afeb4ad5e1a1be8c0d
Fsburyou <burdasovavikusa2006@gmail.com>
2025-03-19 12:12:30 +0300

    Errors was fixed

--- task2var6.py
+++ task2var6.py
@@ -1,7 +1,7 @@
 import holidayapi
 
-key = '08b6d1d3-a60d-4b56-a898-e24e2e705471'
-hapi = holidayapi.v1(key)
+KEY = '08b6d1d3-a60d-4b56-a898-e24e2e705471'
+hapi = holidayapi.v1(KEY)
 holidays = hapi.holidays({
     'country': 'RU',
     'year': '2024',
@@ -12,6 +12,6 @@ ans = holidays['holidays']
 #print(ans)
 
 for i in range(6):
-    print(f'Праздник: {ans[i]['name']} '
-          f'| Дата: {ans[i]['date']} '
-          f'| День недели: {ans[i]['weekday']['date']['name']}')
+    print(f'Праздник: {ans[i]["name"]} '
+          f'| Дата: {ans[i]["date"]} '
+          f'| День недели: {ans[i]["weekday"]["date"]["name"]}')
